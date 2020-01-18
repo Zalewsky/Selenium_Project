bc0c577685d57d1a29b04de0f03323c4cc8f1521
Paweł Zalewski <pawel.zalewsky@yahoo.com>
2020-01-18 14:43:46 +0100

    added chromedriver,set up

---
+++ Project_Selenium/src/test/java/PageObjects/SeleniumTests.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumTests {
+    public WebDriver driver;
+
+    @Before
+    public void SetUp() {
+        System.setProperty("webdriver.chrome.driver" , "chromedriver/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void
+
+
+}
